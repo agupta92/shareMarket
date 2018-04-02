e2f85d24a76c3d28466382eb86937044f133d2e0
unknown <agupta_92@yahoo.co.in.com>
2018-04-03 01:39:06 +0530

    fix include utility

--- script/intraday_adanient.php
+++ script/intraday_adanient.php
@@ -1,8 +1,8 @@
 <?php
 
-include "/../shared/utility.php";
+include (dirname(__FILE__)."/../shared/utility.php");
 $stockCode = 'ADANIENT';
-$output = "Stock : $stockCode\n";
+$output = "Stock : $stockCode<br>";
 $high = '2. high';
 $low = '3. low';
 $result = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=$stockCode&interval=1min&apikey=J6LGP3NREJV0WKUP&outputsize=compact");
@@ -15,16 +15,16 @@ if($result){
 
 //var_dump($result);
 $output .= "Time Considered :";
-$output .= $today = date("Y-m-d 06:00:00");
-//$output .= $today = date("Y-m-d 00:04:00");
+//$output .= $today = date("Y-m-d 00:00:00");
+$output .= $today = date("Y-m-d 00:04:00");
 $property = 'Time Series (1min)';
 //var_dump($result->$property->$today);
 $checkpointValue = $result->$property->$today;
-$output .= "\nHigh: ".$checkpointValue->$high."\nLow: ".$checkpointValue->$low."\n";
+$output .= "<br>High: ".$checkpointValue->$high."<br>Low: ".$checkpointValue->$low."<br>";
 //echo $output;
 $output .= "Order 1 : Buy= ". ($checkpointValue->$high + 0.2) . " & Sell= ". ($checkpointValue->$high + 0.7);
 //echo $output;
-$output .= "\nOrder 2 : Sell= ". ($checkpointValue->$low - 0.2) . " & Buy= ". ($checkpointValue->$low - 0.7);
+$output .= "<br>Order 2 : Sell= ". ($checkpointValue->$low - 0.2) . " & Buy= ". ($checkpointValue->$low - 0.7);
 
 $emailSubject = "ADANIENT DAILY INTRADAY";
 
